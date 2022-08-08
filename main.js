43c51ba4eb7a830fa773266863f2271d49348629
FrederikDiamonD <frederikhandberg02@gmail.com>
2022-08-08 17:51:15 +0200

    Making the navbar for mobile.

---
+++ main.js
@@ -0,0 +1,10 @@
+// Execute script after page load
+window.onload = function digital_fn() {
+    // Toggle button
+    let toggle = document.querySelector('#nav .toggle-btn');
+    let collapse = document.querySelector('#nav .collapse');
+
+    toggle.addEventListener('click', function(event) {
+        collapse.classList.toggle('active')
+    })
+}
